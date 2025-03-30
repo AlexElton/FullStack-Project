a36b393ff2d9c530395e536f9fe334709fa41516
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-03-30 17:08:59 +0200

    Initial commit

---
+++ src/main/java/ntnu/idi/bidata/IDATT2105/Idatt2105Application.java
@@ -0,0 +1,13 @@
+package ntnu.idi.bidata.IDATT2105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idatt2105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idatt2105Application.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/idi/bidata/IDATT2105/Idatt2105ApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idi.bidata.IDATT2105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idatt2105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
