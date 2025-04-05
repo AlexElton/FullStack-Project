e480c013cae967f8aa866f6a3b5ae22537928a6a
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-04-05 15:27:56 +0200

    Feat(messages): Initial view and router function

---
+++ Frontend/market/src/views/MessagesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="messages-page">
+    <!-- Message List Panel -->
+    
+    </div>
+
+    <!-- Message Detail Panel -->
+<div class="message-detail">
+  
+
+   
+  
+ 
+  
+</div>
+  </div>
+</template>
+
