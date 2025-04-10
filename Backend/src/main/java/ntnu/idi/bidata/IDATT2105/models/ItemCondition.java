5f56923115574ca29336d624d1239ea584a3154a
Audun Aakermann <audunaakermann@gmail.com>
2025-04-10 09:48:09 +0900

    Add ItemStatus and ItemCondition enums to be used with Item JPA entity

---
+++ Backend/src/main/java/ntnu/idi/bidata/IDATT2105/models/ItemCondition.java
@@ -0,0 +1,5 @@
+package ntnu.idi.bidata.IDATT2105.models;
+
+public enum ItemCondition {
+  NEW, LIKE_NEW, GOOD, FAIR, POOR
+}
---
+++ Backend/src/main/java/ntnu/idi/bidata/IDATT2105/models/ItemStatus.java
@@ -0,0 +1,5 @@
+package ntnu.idi.bidata.IDATT2105.models;
+
+public enum ItemStatus {
+  DRAFT, ACTIVE, RESERVED, SOLD, ARCHIVED, DELETED
+}
