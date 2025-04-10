469bab6f2b99a8c41ade381b7a38e61258db88d1
Audun Aakermann <audunaakermann@gmail.com>
2025-04-10 22:39:02 +0900

    Add authentication dtos

---
+++ Backend/src/main/java/ntnu/idi/bidata/IDATT2105/dtos/AuthRequestDTO.java
@@ -0,0 +1,43 @@
+package ntnu.idi.bidata.IDATT2105.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Data Transfer Object for authentication requests.
+ */
+public class AuthRequestDTO {
+    
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+    
+    // Default constructor for Jackson
+    public AuthRequestDTO() {
+    }
+    
+    public AuthRequestDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
