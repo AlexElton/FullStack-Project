206444ad88c76dbbb229172591ceb7b2ebd4058f
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2025-04-10 19:18:34 +0200

    Chore(backend): working integration between front- and backend

--- Backend/src/main/java/ntnu/idi/bidata/IDATT2105/config/SecurityConfig.java
+++ Backend/src/main/java/ntnu/idi/bidata/IDATT2105/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package ntnu.idi.bidata.IDATT2105.config;
 
-import ntnu.idi.bidata.IDATT2105.services.security.JwtAuthenticationFilter;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,8 +22,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
+import ntnu.idi.bidata.IDATT2105.services.security.JwtAuthenticationFilter;
 
 /**
  * Spring Security configuration class.
