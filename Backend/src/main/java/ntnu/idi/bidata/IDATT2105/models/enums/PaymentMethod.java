2d328050d423e3bc1b96fcb7cd9f3e5e109b2b65
Audun Aakermann <audunaakermann@gmail.com>
2025-04-10 18:26:29 +0900

    Add Transaction JPA entity, PaymentMethod and TransactionStatus enums

---
+++ Backend/src/main/java/ntnu/idi/bidata/IDATT2105/models/enums/PaymentMethod.java
@@ -0,0 +1,9 @@
+package ntnu.idi.bidata.IDATT2105.models.enums;
+
+/**
+ * Enum representing the possible payment methods.
+ */
+public enum PaymentMethod {
+    VIPPS,
+    OTHER
+}
---
+++ Backend/src/main/java/ntnu/idi/bidata/IDATT2105/models/enums/TransactionStatus.java
@@ -0,0 +1,12 @@
+package ntnu.idi.bidata.IDATT2105.models.enums;
+
+/**
+ * Enum representing the possible statuses of a transaction.
+ */
+public enum TransactionStatus {
+    PENDING,
+    COMPLETED,
+    FAILED,
+    REFUNDED,
+    CANCELLED
+}
